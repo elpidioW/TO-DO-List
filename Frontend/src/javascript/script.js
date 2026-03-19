680e5058d930b3a6e389dc23ae232bf34bd5278a
Wallace <wallace.cardoso206@gmail.com>
2026-03-19 15:24:32 -0300

    Botão de criar tarefa implementado

--- Frontend/src/javascript/script.js
+++ Frontend/src/javascript/script.js
@@ -1,9 +1,10 @@
 
 
+const modalJanela = document.getElementById("janela_modal");
 
 const botaoAbrir = document.getElementById("botao_abrir_modal");
 const botaoFechar = document.getElementById("botao_fechar_modal");
-const modalJanela = document.getElementById("janela_modal");
+const botaoCriarTarefa = document.getElementById("botao_criar_tarefa");
 
 
 
@@ -11,7 +12,10 @@ botaoAbrir.onclick = function(){
     modalJanela.showModal();
 }
 
-
 botaoFechar.onclick = function(){
-    modalJanela.close()
+    modalJanela.close();
+}
+
+botaoCriarTarefa.onclick = function(){
+    console.log("funcionou");
 }
